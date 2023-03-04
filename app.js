5a639be621f9768a5f3313607603838400c9f97a
quaide <tranterq@gmail.com>
2023-03-04 17:46:48 -0500

    Created blog model, added mongoose library, added connection to MongoDB 'Atlast'

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog')
 
 //express app
 const app = express();
+
+//mongoDB connection string
+const dbURI = 'mongodb+srv://nodecourse:t3stp%40ss%21@nodecourse.h4qkmfb.mongodb.net/nodeCourse?retryWrites=true&w=majority';
+mongoose.connect(dbURI)
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
 
 //register view engine
 app.set('view engine', 'ejs');
 //app.set('views', 'myviews');
 
 //listen for requests
-app.listen(3000);
+//app.listen(3000);
 
 //hard code alternative to the morgan library
 // app.use((req, res, next) => {
@@ -23,6 +31,23 @@ app.listen(3000);
 //middleware & static files
 app.use(express.static('public'));
 app.use(morgan('dev'));
+
+//mongoose and mongo sandbox routes, test interaction with db
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog( {
+        title: 'New Blog',
+        snippet: 'About my New Blog',
+        body: 'Blog Body'
+    });
+
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.logg(err);
+        })
+})
 
 app.get('/', (req, res) => {
     //res.send('<p>home page</p>');
