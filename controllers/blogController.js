fc2fc58354cb8027ef4a40d8334df86eba99b599
quaide <tranterq@gmail.com>
2023-03-05 14:42:10 -0500

    model, view, controller to separate responsibiliy and for cleaner code

---
+++ controllers/blogController.js
@@ -0,0 +1,60 @@
+const Blog = require('../models/blog');
+
+//blogIndex, blogDetails, blogCreate, blogCreateGet, blogCreatePost, blogDelete
+
+const blogIndex = (req, res) => {
+    Blog.find().sort({createdAt: -1})
+    .then((result) => {
+        res.render('index', { title: 'All Blogs', blogs: result})
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
+const blogDetails = (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+    .then(result => {
+        res.render('details', { blog: result, title: 'Post Details' });
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+
+const blogCreateGet = (req, res) => {
+    res.render('create', { title: 'Create a New Blog'});
+}
+
+const blogCreatePost = (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs');
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
+
+const blogDelete = (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/blogs'});
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+module.exports = {
+    blogIndex,
+    blogDetails,
+    blogCreateGet,
+    blogCreatePost,
+    blogDelete
+}
---
+++ routes/blogRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const blogController = require('../controllers/blogController.js')
+
+const router = express.Router();
+
+router.get('/', blogController.blogIndex);
+
+router.post('/', blogController.blogCreatePost);
+
+router.get('/create', blogController.blogCreateGet);
+
+router.get('/:id', blogController.blogDetails);
+
+router.delete('/:id', blogController.blogDelete);
+
+module.exports = router;
